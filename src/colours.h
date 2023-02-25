b03a3effb309c9a756bab83b136f293ce1013802
KryptosRepublic <fisher_phisher@protonmail.com>
2023-02-24 20:07:10 -0500

    feat: colours.h to organize colours

---
+++ src/colours.h
@@ -0,0 +1,15 @@
+#ifndef COLOURS_H
+#define COLOURS_H
+
+#define RED     "\x1b[31m"
+#define GREEN   "\x1b[32m"
+#define YELLOW  "\x1b[33m"
+#define BLUE    "\x1b[34m"
+#define MAGENTA "\x1b[35m"
+#define CYAN    "\x1b[36m"
+#define RESET   "\x1b[0m"
+
+#define BOLD(color) "\x1b[1m" color
+
+#endif /* COLOURS_H */
+
